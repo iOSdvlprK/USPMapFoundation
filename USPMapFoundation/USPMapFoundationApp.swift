bc05aaed76f20cb8d3c03360ed0ef936aaaad210
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-11-22 23:11:52 +0900

    Initial Commit

---
+++ USPMapFoundation/USPMapFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPMapFoundationApp.swift
+//  USPMapFoundation
+//
+//  Created by joe on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct USPMapFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
