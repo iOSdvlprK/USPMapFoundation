298e8172a6ee61c0a7cf92771152d2a63fc4a772
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-11-23 22:13:19 +0900

    add Map view with example markers
    - displays markers for coffee and restaurant locations using predefined coordinates
    - introduces a CLLocationCoordinate2D extension for easy access to these coordinates

--- USPMapFoundation/ContentView.swift
+++ USPMapFoundation/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static var coffee: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 30.011180, longitude: -95.512560)
+    }
+    
+    static var restaurant: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 29.988869677028426, longitude: -95.55595575564956)
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map {
+            Marker("Coffee", coordinate: .coffee)
+            Marker("Restaurant", coordinate: .restaurant)
         }
-        .padding()
     }
 }
 
